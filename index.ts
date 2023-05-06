a159c087c9bb56ab49818125ef7c736a6b7a4cbf
Awais Khalil <126592168+Awais-Khalil@users.noreply.github.com>
2023-05-06 18:19:54 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+function countdown(targetDate: Date): void {
+    const interval = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const timeLeft = targetDate.getTime() - currentTime;
+      if (timeLeft <= 0) {
+        clearInterval(interval);
+        console.log("Countdown finished!");
+      } else {
+        const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hoursLeft = Math.floor(
+          (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutesLeft = Math.floor(
+          (timeLeft % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        const secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        console.log(
+          `Countdown: ${daysLeft}d ${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`
+        );
+      }
+    }, 1000);
+  }
+  
+  // Example usage: countdown to Dec 25, 2023 at 12:00:00
+  const targetDate = new Date("Dec 25, 2023 12:00:00");
+  countdown(targetDate);
+  
